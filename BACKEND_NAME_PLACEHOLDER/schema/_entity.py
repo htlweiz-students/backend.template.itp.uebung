bc2983f47e70a607c2b5928451af75479337c463
Robert Ulmer <robert.ulmer@lr.htlweiz.at>
2026-04-01 17:00:54 +0200

    add docstring for schema

--- BACKEND_NAME_PLACEHOLDER/schema/_entity.py
+++ BACKEND_NAME_PLACEHOLDER/schema/_entity.py
@@ -1,14 +1,49 @@
+"""
+A module containing Pydantic models for managing entities.
+"""
+
 from pydantic import BaseModel
 
+__all__ = ['EntityBase', 'EntityFull', 'EntityFilter']
 
+"""
+Base model for an entity, containing the name attribute.
+"""
 class EntityBase(BaseModel):
+    """
+    A Pydantic base model for an entity with a required `name` attribute of type string.
+
+    Attributes:
+        name (str): The name of the entity.
+    """
     name: str
 
-
+"""
+A full entity model, extending EntityBase and adding an `id` attribute.
+"""
 class EntityFull(EntityBase):
+    """
+    A Pydantic model for a full entity, inheriting from EntityBase and adding an `id` attribute of
+    type integer.
+
+    Attributes:
+        id (int): The unique identifier of the entity.
+        name (str): The name of the entity.
+    """
     id: int
 
-
+"""
+A filter model for entities, allowing optional `name` and `id` attributes.
+"""
 class EntityFilter(BaseModel):
+    """
+    A Pydantic model for a filter on entities, allowing optional `name` and `id` attributes. Both
+    can be of type string or integer.
+
+    Attributes:
+        name (str | None): The optional name to filter by.
+        id (int | None): The optional identifier to filter by.
+    """
     name: str | None
     id: int | None
+
--- BACKEND_NAME_PLACEHOLDER/schema/_user.py
+++ BACKEND_NAME_PLACEHOLDER/schema/_user.py
@@ -1,11 +1,34 @@
+"""
+Module containing User data models for application.
+
+This module defines two classes: UserBase and UserFull, both derived from Pydantic's BaseModel.
+These classes represent the structure of user data in the application, with UserBase as a base model
+containing common attributes like user_name, name, and password_hash, and UserFull extending it by
+adding an id attribute.
+"""
+
 from pydantic import BaseModel
 
-
 class UserBase(BaseModel):
+    """
+    Represents the base structure of a User object in the application.
+
+    Attributes:
+        user_name (str): The username for the user.
+        name (str): The full name of the user.
+        password_hash (str): The hashed password of the user.
+    """
     user_name: str
     name: str
     password_hash: str
 
-
 class UserFull(UserBase):
+    """
+    Represents a complete User object in the application, inheriting from UserBase and adding an
+    id attribute.
+
+    Attributes:
+        id (int): The unique identifier for the user.
+    """
     id: int
+
